694fb0acbc25490517210a86c30a3dac532fe8ec
vilodek5 <152364231+vilodek5@users.noreply.github.com>
2024-12-15 23:38:30 +0500

    Готова 5 работа

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Vilodya/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function addCard (card) {
+  const cardNode = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const image = cardNode.querySelector('.card__image');
+  const title = cardNode.querySelector('.card__title');
+  const deleteButton = cardNode.querySelector('.card__delete-button');
+
+  image.src = card.link;
+  image.alt = card.name;
+  title.textContent = card.name;
+
+  deleteButton.addEventListener("click", () => removeCard(cardNode));
+
+  return cardNode;
+}
 
 // @todo: Функция удаления карточки
+function removeCard(cardNode) {
+  cardNode.remove();
+}
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(card => {
+  const cardElement = addCard(card);
+  placesList.append(cardElement);
+});
