9020e38c5919525a2c365aba676fad3d0c049cea
Vilodya <rygyj95@gmail.com>
2025-02-24 01:50:01 +0500

    Начата валидация форм

--- src/index.js
+++ src/index.js
@@ -17,6 +17,27 @@ const previewModal = document.querySelector('.popup_type_image');
 const previewModalImage = previewModal.querySelector('.popup__image');
 const previewModalCaption = previewModal.querySelector('.popup__caption');
 
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+// Находим форму в DOM
+const formEditProfile = document.forms["edit-profile"];
+// Находим поля формы в DOM
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_description');
+
+// Находим форму в DOM
+const formNewPlace = document.forms["new-place"];
+// Находим поля формы в DOM
+const cardNameInput = document.querySelector('.popup__input_type_card-name');
+const imageUrlInput = document.querySelector('.popup__input_type_url');
+
+const cardList = document.querySelector('.places__list');
+
+// @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+
+// Функции
 function openPreview(name, link) {
   previewModalImage.src = link;
   previewModalImage.alt = name;
@@ -24,6 +45,30 @@ function openPreview(name, link) {
   openModal(previewModal);
 }
 
+function submitEditProfileForm(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal(modalEdit);
+}
+
+function createCard(evt){
+  evt.preventDefault();
+  const newCard = addCard(
+    {
+      name: cardNameInput.value,
+      link: imageUrlInput.value
+    },
+    removeCard,
+    handleLike,
+    openPreview
+  );
+  cardList.prepend(newCard);
+  evt.target.reset();
+  closeModal(modalNewCard);
+}
+
+// Обработчики событий
 buttonEdit.addEventListener("click", function () {
   openModal(modalEdit);
 });
@@ -45,51 +90,9 @@ modals.forEach(modal => {
   });
 });
 
-// Находим форму в DOM
-const formEditProfile = document.forms["edit-profile"];
-// Находим поля формы в DOM
-const nameInput = document.querySelector('.popup__input_type_name');
-const jobInput = document.querySelector('.popup__input_type_description');
-const profileTitle = document.querySelector('.profile__title');
-const profileDescription = document.querySelector('.profile__description');
-
-function submitEditProfileForm(evt) {
-    evt.preventDefault();
-    profileTitle.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
-    closeModal(modalEdit);
-}
-
 formEditProfile.addEventListener('submit', submitEditProfileForm);
 
-// Находим форму в DOM
-const formNewPlace = document.forms["new-place"];
-// Находим поля формы в DOM
-const cardNameInput = document.querySelector('.popup__input_type_card-name');
-const imageUrlInput = document.querySelector('.popup__input_type_url');
-
-const cardList = document.querySelector('.places__list');
-
-function createCard(evt){
-  evt.preventDefault();
-  const newCard = addCard(
-    {
-      name: cardNameInput.value,
-      link: imageUrlInput.value
-    },
-    removeCard,
-    handleLike,
-    openPreview
-  );
-  cardList.prepend(newCard);
-  evt.target.reset();
-  closeModal(modalNewCard);
-}
-
 formNewPlace.addEventListener('submit', createCard);
-
-// @todo: Вывести карточки на страницу
-const placesList = document.querySelector('.places__list');
 
 initialCards.forEach(card => {
   const cardElement = addCard(card, removeCard, handleLike, openPreview);
